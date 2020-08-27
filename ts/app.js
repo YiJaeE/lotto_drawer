4724c202a55f9949b92e9c0e099884520440637a
YiJaeE <jinnn2181@gmail.com>
2020-08-27 16:43:06 +0900

    feat: Improved JavaScript Code to TypeScript

---
+++ ts/app.js
@@ -0,0 +1,46 @@
+"use strict";
+var $selectLotto = document.querySelector('.select-lotto');
+var $lotto = document.querySelector('.lotto');
+var draw = function () {
+    var lotto = [];
+    var _loop_1 = function (i) {
+        var num = Math.floor(Math.random() * 45) + 1;
+        var duplicate = lotto.every(function (n) { return n !== num; });
+        if (duplicate) {
+            lotto.push(num);
+        }
+        else {
+            i--;
+        }
+        lotto.sort(function (a, b) { return a - b; });
+        out_i_1 = i;
+    };
+    var out_i_1;
+    for (var i = 0; i < 6; i++) {
+        _loop_1(i);
+        i = out_i_1;
+    }
+    var $lottoList = document.createElement('li');
+    $lottoList.setAttribute('id', 'lotto-number');
+    var $lottoNumber = document.createTextNode("" + lotto);
+    $lottoList.appendChild($lottoNumber);
+    $lotto !== null && $lotto.appendChild($lottoList);
+};
+$selectLotto !== null && $selectLotto.addEventListener('click', function (e) {
+    var target = e.target;
+    var $removeContainer = document.getElementById('lotto-number');
+    if ($removeContainer !== null && $lotto !== null) {
+        for (var i = 0; i < $lotto.children.length; i++) {
+            var num = $lotto.children.length;
+            while (num > 0) {
+                if ($lotto.children[i]) {
+                    $lotto.removeChild($lotto.children[i]);
+                }
+                num--;
+            }
+        }
+    }
+    for (var i = 0; i < +target.id; i++) {
+        draw();
+    }
+});
---
+++ ts/app.ts
@@ -0,0 +1,40 @@
+const $selectLotto: Element | null = document.querySelector('.select-lotto');
+const $lotto: Element | null = document.querySelector('.lotto');
+
+const draw = () => {
+  const lotto = [];
+  for (let i = 0; i < 6; i++) {
+    const num = Math.floor(Math.random() * 45) + 1;
+    const duplicate = lotto.every(n => n !== num);
+    if (duplicate) {
+      lotto.push(num);
+    } else {
+      i--;
+    }
+    lotto.sort((a, b) => a - b);
+  }
+  const $lottoList = document.createElement('li');
+  $lottoList.setAttribute('id', 'lotto-number');
+  const $lottoNumber = document.createTextNode(`${lotto}`);
+  $lottoList.appendChild($lottoNumber);
+  $lotto !== null && $lotto.appendChild($lottoList);
+};
+
+$selectLotto !== null && $selectLotto.addEventListener('click', (e: Event) => {
+  const target: HTMLElement = <HTMLElement>e.target;
+  const $removeContainer = document.getElementById('lotto-number');
+  if ($removeContainer !== null && $lotto !== null) {
+    for (let i = 0; i < $lotto.children.length; i++) {
+      let num = $lotto.children.length;
+      while (num > 0) {
+        if ($lotto.children[i]) {
+          $lotto.removeChild($lotto.children[i]);
+        }
+        num--;
+      }
+    }
+  }
+  for (let i = 0; i < +target.id; i++) {
+    draw();
+  }
+});
